8dbc360df945d1b4d3274e752c2939e03712ae8d
Justin Parrondo <justinparrondo@gmail.com>
2021-06-06 01:23:02 -0400

    Initial commit

---
+++ src/main/java/org/cop3330/App.java
@@ -0,0 +1,24 @@
+package org.cop3330;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("How many people? ");
+        int numPeople = in.nextInt();
+        System.out.print("How many pizzas do you have? ");
+        int numPizzas = in.nextInt();
+        System.out.print("How many slices per pizza? ");
+        int slicesPerPizza = in.nextInt();
+
+        int numSlices = numPizzas * slicesPerPizza;
+        int slicesPerPerson = numSlices / numPeople;
+        int leftoverSlices = numSlices % numPeople;
+
+        System.out.printf("%d people with %d pizzas (%d slices)\n", numPeople, numPizzas, numSlices);
+        System.out.printf("Each person gets %d pieces of pizza.\n", slicesPerPerson);
+        System.out.printf("There are %d leftover pieces.\n", leftoverSlices);
+    }
+}
